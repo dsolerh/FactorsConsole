0a09420565b1ed0ab07b262caaa06abac61d5fcd
dsolerh <dsolerh.cinter95@gmail.com>
2021-10-15 20:30:43 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactorsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
